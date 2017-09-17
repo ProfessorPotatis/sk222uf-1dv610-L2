42df835436a6aced2b8347970f0396552f70c962
Sofia Kristiansen <sk222uf@student.lnu.se>
2017-09-17 13:05:26 +0200

    Update DBConfig.php

--- model/DBConfig.php
+++ model/DBConfig.php
@@ -1,13 +1,6 @@
 <?php
 
 $db_host = 'localhost';
-
-// ON PRODUCTION SITE ONLY!!
-/*$db_user = 'id2850916_ppdb';
-$db_password = 's0S3cRetOMG';
-$db_name = 'id2850916_professorpotatisdb';*/
-
-// ON LOCAL MACHINE ONLY!!
-$db_user = 'root';
-$db_password = 'root';
-$db_name = 'userDB';
+$db_user = 'user';
+$db_password = 'password';
+$db_name = 'databaseName';
